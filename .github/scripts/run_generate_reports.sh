9f6d07a7671ff06a2037b96b827537ec11eea7a1
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2023-11-23 17:19:08 +0100

    Added logging inside script when it's done

--- .github/scripts/run_generate_reports.sh
+++ .github/scripts/run_generate_reports.sh
@@ -7,5 +7,6 @@ echo "Current working directory: $(pwd)"
 echo "Contents of current working directory: $(ls -a)"
 mkdir -p ./report
 node ./index.js
+echo "Contents of the report folder: $(ls -al ./report)"
 echo "Finished running generate reports script"
 
