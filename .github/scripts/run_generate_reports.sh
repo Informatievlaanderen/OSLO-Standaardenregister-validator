2c2f11f1fa8249c0d6ae95a0e96131d09ced4cc0
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2023-11-23 16:35:25 +0100

    Updated node starting point inside runner

--- .github/scripts/run_generate_reports.sh
+++ .github/scripts/run_generate_reports.sh
@@ -3,8 +3,8 @@
 # Generate the reports for the standards validator
 
 echo "Running generate reports script"
-pwd
-ls -a
-node ../../index.js
+echo "Current working directory: $(pwd)"
+echo "Contents of current working directory: $(ls -a)"
+node ./index.js
 echo "Finished running generate reports script"
 
