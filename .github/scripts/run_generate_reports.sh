13466adf3c26f8049ae026d0ad1122cdefcd2f7c
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2023-11-23 16:30:00 +0100

    Added some echo scripts to see if the folder dir is correct

--- .github/scripts/run_generate_reports.sh
+++ .github/scripts/run_generate_reports.sh
@@ -3,6 +3,8 @@
 # Generate the reports for the standards validator
 
 echo "Running generate reports script"
+echo pwd
+echo ls -a
 node ../../index.js
 echo "Finished running generate reports script"
 
